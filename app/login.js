066dbb9135209f731ba7c288dd0597d721a9f9ca
Gyorgy Gabor <gyorgyg@gmail.com>
2017-01-27 09:27:59 +0100

    unneccessarily login modification

--- app/login.js
+++ app/login.js
@@ -1,34 +1,50 @@
-module.exports = angular.module('LoginController', ['ngRoute', 'ngAnimate']).controller('LoginController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+(function (){
+  var app = angular.module('LoginController', ['ngRoute', 'ngAnimate']).config(['$httpProvider', function ($httpProvider) {
+      //Reset headers to avoid OPTIONS request (aka preflight)
+      $httpProvider.defaults.headers.common = {};
+      $httpProvider.defaults.headers.post = {};
+      $httpProvider.defaults.headers.get = {};
+      $httpProvider.defaults.headers.delete = {};
+      $httpProvider.defaults.headers.put = {};
+      $httpProvider.defaults.headers.patch = {};
+      // $httpProvider.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+      //$httpProvider.defaults.headers.common['Authorization'] = 'Basic ' + btoa(my_id+ ':' + my_pass")
+     }]);
 
-  $scope.token={}
 
-  $scope.login = function() {
-    if ($scope.user.email !== '' && $scope.user.password !== '') {
-      $http({
-        method: 'POST',
-        data: {
-          email: $scope.user.email,
-          password: $scope.user.password,
-        },
-        url: 'http://localhost:3000/user/login',
-      }).then(function (data) {
-        var respond = (data.data);
-        if (respond.result === 'success') {
-          // console.log(respond.token);
-          // console.log(respond);
-          localStorage.setItem("token", respond.token);
-          // console.log(localStorage);
-          $location.path('/home');
-        }
-      }).catch(function (data) {
-        console.log('error');
-      });
-    }
-  };
-  $scope.signUpView = function () {
-    $location.path('/signup');
-  };
-}]);
+  app.controller('LoginController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
+    $scope.token={}
+
+    $scope.login = function() {
+      if ($scope.user.email !== '' && $scope.user.password !== '') {
+        $http({
+          method: 'POST',
+          data: {
+            email: $scope.user.email,
+            password: $scope.user.password
+          },
+          url: 'https://taskmanager-backend.gomix.me/:3000/login',
+        }).then(function (data) {
+          var respond = (data.data);
+          console.log(respond)
+          if (respond.result === 'success') {
+            // console.log(respond.token);
+            // console.log(respond);
+            localStorage.setItem("token", respond.token);
+            console.log(localStorage);
+            $location.path('/home');
+          }
+        }).catch(function (data) {
+          console.log('error');
+        });
+      }
+    };
+    $scope.signUpView = function () {
+      $location.path('/signup');
+    };
+  }]);
+  module.exports = app: app
+})();
 
 // LoginController;
